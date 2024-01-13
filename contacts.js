c013629cac5f98d963fb35cfaf010c5b10d37c23
Veronika Timakova <tymnik@ukr.net>
2024-01-13 22:07:07 +0200

    template

---
+++ contacts.js
@@ -0,0 +1,19 @@
+const fs = require("fs").promises;
+
+const contactsPath = require("./db/contacts.json");
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
